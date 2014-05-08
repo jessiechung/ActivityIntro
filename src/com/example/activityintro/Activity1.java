045a80c1a31cc72790bd52d1e9d42c349ab0ad76
jessiechung <keiko198388@gmail.com>
2014-05-08 19:41:27 +0800

    init

---
+++ src/com/example/activityintro/Activity1.java
@@ -0,0 +1,11 @@
+package com.example.activityintro;
+
+import android.app.Activity;
+
+
+public class Activity1 extends Activity {
+	private void onCreate() {
+		// TODO Auto-generated method stub
+
+	}
+}
